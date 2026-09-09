2778c04f651087d44e04f4867f6fd9c77216f848
WPEssential <wordpress.essential@gmail.com>
2026-09-10 00:57:14 +0500

    Remove selling-related commercial service

--- commercial-service/app/community/page.tsx
+++
@@ -1,8 +0,0 @@
-import { Suspense } from "react";
-import CommunityClient from "./CommunityClient";
-
-export default function CommunityPage() {
-  return <Suspense fallback={<main style={{ padding: 24 }}>Loading ANPOS Community…</main>}>
-    <CommunityClient />
-  </Suspense>;
-}
